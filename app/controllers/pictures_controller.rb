c186f27563fb92aab843d499889283dac76aea61
Jonathan Lam <jonathanryanlam@gmail.com>
2015-02-03 16:53:23 -0500

    added delete and edit button but need to refactor

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,17 +1,55 @@
 class PicturesController < ApplicationController
-	def index
-    @pictures = Picture.all
-	end
 
-	def show
+  def index
+  	@pictures = Picture.all 
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def show
+	@picture = Picture.find(params[:id])
+  end
+
+
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
+
+   def edit
     @picture = Picture.find(params[:id])
-  	end
+  end
 
-  	def new
-  	end
+  def update
+    @picture = Picture.find(params[:id])
 
-  	def create
-  		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-  	end
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+   def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
+
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 end
-
